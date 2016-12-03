affebf999dc6d8c30607a8d28ea690786df4690f
Brian Blain <bblain7@gmail.com>
2016-12-02 18:25:11 -0800

    Made the installation more dynamic
    The original ubuntu-server-16.04.json was the Packer script has been changed to just "packer.json". The ubuntu-server-16.04 file now contains the download url properties to send into the Packer configuration. Instead of providing the iso from the machine, the url now downloads the iso file.

--- ubuntu-server-16.04.sh
+++ ubuntu-server-16.04.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-
 dir=$(dirname "$(readlink -f "$0")")
-cd ${dir}
-
 vm_provider="virtualbox"
 box_name="ubuntu-server-16.04"
 
-packer build ${box_name}.json || {
+packer build -var "box_name=${box_name}" -var-file="${dir}/${box_name}.json" "${dir}/packer.json" || {
   echo "Packer failed to create Vagrant box.";
   exit 1;
 }
 
-vagrant box add --force ${USER}/${box_name} ${dir}/boxes/${vm_provider}/${box_name}.box || {
+vagrant box add --force "${USER}/${box_name}" "${dir}/boxes/${vm_provider}/${box_name}.box" || {
   echo "Vagrant failed to add box.";
   exit 1;
 }
-
